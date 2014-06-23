29a21838f14d04818f172e3400aa858e958e0b0b
courington <chase.courington@gmail.com>
2014-06-23 00:01:17 -0600

    hello world with express, swig and mongodb

--- hello_express/app.js
+++ hello_express/app.js
@@ -1,18 +1,36 @@
 var express = require('express'),
     app = express(),
-    cons = require('consolidate');
+    cons = require('consolidate'),
+    MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
+var mongoclient = new MongoClient(new Server('localhost', 27017,
+    {'native_parser' : true}));
+
+var db = mongoclient.db('course');
+
 app.get('/', function(req, res) {
-    res.render('hello', {'name' : 'Swig'});
+    db.collection('hello_mongo_express').findOne({}, function(err, doc) {
+
+        if(err) throw err;
+
+        res.render('hello', doc);
+
+    });
 });
 
 app.get('*', function(req, res) {
     res.send('Page Not Found!', 404);
 });
 
-app.listen(8080);
-console.log('server started at http://localhost:8080');
+mongoclient.open(function(err, mongoclient) {
+    
+    if(err) throw err;
+
+    app.listen(8080);
+    console.log('server started at http://localhost:8080');
+});
